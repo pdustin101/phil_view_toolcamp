ebe505c155f3498cc67b25e83db297107ce5136d
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 17:06:24 +0000

    Dev tool Inital commit

---
+++ lib/phil_view_toolcamp.rb
@@ -0,0 +1,6 @@
+require "phil_view_toolcamp/version"
+require "phil_view_toolcamp/renderer"
+
+module PhilViewToolcamp
+  # Your code goes here...
+end
---
+++ lib/phil_view_toolcamp/version.rb
@@ -0,0 +1,3 @@
+module PhilViewToolcamp
+  VERSION = "0.1.0"
+end
